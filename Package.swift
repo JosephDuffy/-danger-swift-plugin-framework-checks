19a6298dc882b26bd5444ea80a17d1fd97e6d8a9
Joseph Duffy <commits@josephduffy.co.uk>
2022-02-06 10:45:57 -0800

    Update to Danger 3.12.3 and Swift 5.5

--- Package.swift
+++ Package.swift
@@ -1,22 +1,22 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
     name: "SwiftChecksDangerPlugin",
     platforms: [
-        .macOS(.v10_10),
+        .macOS(.v10_12),
     ],
     products: [
         .library(name: "SwiftChecksDangerPlugin", type: .dynamic, targets: ["SwiftChecksDangerPlugin"]),
     ],
     dependencies: [
-        .package(name: "danger-swift", url: "https://github.com/danger/swift.git", from: "3.0.0"),
+        .package(name: "danger-swift", url: "https://github.com/danger/swift.git", from: "3.12.3"),
     ],
     targets: [
         .target(
             name: "SwiftChecksDangerPlugin",
             dependencies: [
-                .product(name: "Danger", package: "danger-swift")
+                .product(name: "Danger", package: "danger-swift"),
             ]
         ),
     ],
